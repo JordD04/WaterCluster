d6d3bbc711f6b49847a98efc9b299a221e8401fd
JordD04 <JordanDorrell@hotmail.com>
2018-06-15 18:47:27 +0100

    Update splitter2.py

--- splitter2.py
+++ splitter2.py
@@ -2,23 +2,30 @@ fileNForm = input('\nEnter generic form of file name: ')                   # fin
 noFiles = input('\nEnter number of files: ')
 fileExt = input('\nEnter file extension: ')
 noTemp = input('\nEnter number of discrete temperature values: ')          # finds number of discrete temperature values
-outputFile = ''.join([fileNForm, '.csv'])                                  # creates name of output 
+outputFile = ''.join([fileNForm, '.csv'])                                  # creates name of output
 outputFileOpen = open(outputFile, 'w')
 
 
-jList = [[] for i in range(noTemp)]
-n = 0
+jList = [[] for i in range(int(noTemp))]
+n = 0                                                                      # n = number of the file
 while n < int(noFiles):
-    inputFile = ''.join([fileNForm, n, fileExt])                           # creates name of input file
+    inputFile = ''.join([fileNForm, str(n), fileExt])                      # creates name of input file
     lines = [line.rstrip('\n') for line in open(inputFile)]                # splits lines into individual variables
 
-    x=2
-    while x<203:                                                           # replaces all spaces with commas
+    x=2                                                                    # x = row of file
+    while x<int(noTemp)+2:                                                 # replaces all spaces with commas
+        print(lines[x])
         lineSplit = lines[x].split(' ')
         heatCap = lineSplit[3]
         jList[x-2].append([heatCap])
         x=x+1
     n=n+1
 
+noTempCount = 0
+while noTempCount < int(noTemp):
+        activeRowList = jList[noTempCount]
+        newline = ', '.join(activeRowList)
+        outputFileOpen.write(newline)
+        noTempCount = noTempCount+1
 
 outputFileOpen.close()
